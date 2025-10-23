cdae8cef42f2ed63a0f3cd3436ac34f78ad06f09
SR <saodat.riskiyeva@gmail.com>
2025-10-22 23:56:01 -0400

    minor code refinement

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -6,35 +6,6 @@ test.beforeEach(async ({ page }) => {
   await page.getByText("Form Layouts").click();
 });
 
-// test("locator syntax rules", async ({ page }) => {
-//   // by tag name
-//   await page.locator("input");
-
-//   // by id
-//   await page.locator("#inputEmail1");
-
-//   // by class name
-//   await page.locator(".shape-rectangle");
-
-//   // by attribute
-//   await page.locator("[placeholder='Email']");
-
-//   // by Class value
-//   await page.locator("[class='input-full-width size-medium shape-rectangle']");
-
-//   // combine selectors
-//   await page.locator("input[placeholder='Email'].shape-rectangle");
-
-//   // by XPath
-//   await page.locator("//input[@placeholder='Email']");
-
-//   // by partial text match
-//   await page.locator(":text('Sign in')");
-
-//   // by exact text match
-//   await page.locator(":text-is('Sign in')");
-// });
-
 test("interact with elements by roles", async ({ page }) => {
   await page.getByRole("textbox", { name: "Email" }).first().click();
   await page.getByRole("button", { name: "Sign in" }).first().click();
