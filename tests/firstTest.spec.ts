fcb292209e68801020bed6d8baa1791f2a88f928
SR <saodat.riskiyeva@gmail.com>
2025-10-24 16:54:01 -0400

    User facing locators

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -6,31 +6,44 @@ test.beforeEach(async ({ page }) => {
   await page.getByText("Form Layouts").click();
 });
 
-test("locator syntax rules", async ({ page }) => {
-  // by tag name
-  await page.locator("input");
+// test("locator syntax rules", async ({ page }) => {
+//   // by tag name
+//   await page.locator("input");
 
-  // by id
-  await page.locator("#inputEmail1");
+//   // by id
+//   await page.locator("#inputEmail1");
 
-  // by class name
-  await page.locator(".shape-rectangle");
+//   // by class name
+//   await page.locator(".shape-rectangle");
 
-  // by attribute
-  await page.locator("[placeholder='Email']");
+//   // by attribute
+//   await page.locator("[placeholder='Email']");
 
-  // by Class value
-  await page.locator("[class='input-full-width size-medium shape-rectangle']");
+//   // by Class value
+//   await page.locator("[class='input-full-width size-medium shape-rectangle']");
 
-  // combine selectors
-  await page.locator("input[placeholder='Email'].shape-rectangle");
+//   // combine selectors
+//   await page.locator("input[placeholder='Email'].shape-rectangle");
 
-  // by XPath
-  await page.locator("//input[@placeholder='Email']");
+//   // by XPath
+//   await page.locator("//input[@placeholder='Email']");
 
-  // by partial text match
-  await page.locator(":text('Sign in')");
+//   // by partial text match
+//   await page.locator(":text('Sign in')");
 
-  // by exact text match
-  await page.locator(":text-is('Sign in')");
+//   // by exact text match
+//   await page.locator(":text-is('Sign in')");
+// });
+
+test("interact with elements by roles", async ({ page }) => {
+  await page.getByRole("textbox", { name: "Email" }).first().click();
+  await page.getByRole("button", { name: "Sign in" }).first().click();
+
+  await page.getByLabel("Email").first().click();
+  await page.getByPlaceholder("Jane Doe").click();
+
+  await page.getByText("Using the Grid").click();
+  await page.getByTitle("IoT Dashboard").click();
+
+  //   await page.getByTestId("").click();
 });
