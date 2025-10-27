b1f8b975f990756dd3f3b00cb1a24d380f734f06
SR <saodat.riskiyeva@gmail.com>
2025-10-27 07:14:19 -0400

    updating package.json

--- src/app/pages/tables/tables.module.ts
+++ src/app/pages/tables/tables.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { NbCardModule, NbIconModule, NbInputModule, NbTreeGridModule } from '@nebular/theme';
-import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { NgModule } from "@angular/core";
+import {
+  NbCardModule,
+  NbIconModule,
+  NbInputModule,
+  NbTreeGridModule,
+} from "@nebular/theme";
+import { Angular2SmartTableModule } from "angular2-smart-table";
 
-import { ThemeModule } from '../../@theme/theme.module';
-import { TablesRoutingModule, routedComponents } from './tables-routing.module';
-import { FsIconComponent } from './tree-grid/tree-grid.component';
+import { ThemeModule } from "../../@theme/theme.module";
+import { TablesRoutingModule, routedComponents } from "./tables-routing.module";
+import { FsIconComponent } from "./tree-grid/tree-grid.component";
 
 @NgModule({
   imports: [
@@ -16,9 +21,6 @@ import { FsIconComponent } from './tree-grid/tree-grid.component';
     TablesRoutingModule,
     Ng2SmartTableModule,
   ],
-  declarations: [
-    ...routedComponents,
-    FsIconComponent,
-  ],
+  declarations: [...routedComponents, FsIconComponent],
 })
-export class TablesModule { }
+export class TablesModule {}
